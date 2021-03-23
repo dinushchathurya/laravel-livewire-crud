87c02be57552f6fc4d34235794a78327964c7edc
dinushchathurya <dinushchathurya21@gmail.com>
2021-03-23 09:00:27 +0530

    refactor: send user data to the users components

--- app/Http/Livewire/Users.php
+++ app/Http/Livewire/Users.php
@@ -7,8 +7,12 @@ use App\User;
 
 class Users extends Component
 {
+    public $users,$name,$email,$user_id;
+    public $updateMode = false;
+
     public function render()
-    {
+    {    
+        $this->users = User::all();    
         return view('livewire.users');
     }
 }
