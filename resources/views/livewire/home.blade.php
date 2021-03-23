77515a6efab5d36bed64da7583ce0916f58331d2
dinushchathurya <dinushchathurya21@gmail.com>
2021-03-23 19:46:38 +0530

    refactor: change div class form md-8 to md-12

--- resources/views/livewire/home.blade.php
+++ resources/views/livewire/home.blade.php
@@ -8,7 +8,7 @@
 <body>
     <div class="container">
         <div class="row justify-content-center">
-            <div class="col-md-8">
+            <div class="col-md-12">
                 <div class="card">
                     <div class="card-header">
                         <h2>Laravel Livewire Crud</h2>
