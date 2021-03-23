864b429fc5f27ccd5753039f3d5eda38c1fcc02f
dinushchathurya <dinushchathurya21@gmail.com>
2021-03-23 17:28:33 +0530

    feat: create user create, edit and delete functions

--- app/Http/Livewire/Users.php
+++ app/Http/Livewire/Users.php
@@ -20,4 +20,65 @@ class Users extends Component
         $this->name = '';
         $this->email = '';
     }
+
+     public function store()
+    {
+        $validatedDate = $this->validate([
+            'name' => 'required',
+            'email' => 'required|email',
+        ]);
+
+        User::create($validatedDate);
+
+        session()->flash('message', 'Users Created Successfully.');
+
+        $this->resetInputFields();
+
+    }
+
+    public function edit($id)
+    {
+        $this->updateMode = true;
+        $user = User::where('id',$id)->first();
+        $this->user_id = $id;
+        $this->name = $user->name;
+        $this->email = $user->email;
+        
+    }
+
+    public function cancel()
+    {
+        $this->updateMode = false;
+        $this->resetInputFields();
+
+
+    }
+
+    public function update()
+    {
+        $validatedDate = $this->validate([
+            'name' => 'required',
+            'email' => 'required|email',
+        ]);
+
+        if ($this->user_id) {
+            $user = User::find($this->user_id);
+            $user->update([
+                'name' => $this->name,
+                'email' => $this->email,
+            ]);
+            $this->updateMode = false;
+            session()->flash('message', 'Users Updated Successfully.');
+            $this->resetInputFields();
+
+        }
+    }
+
+    public function delete($id)
+    {
+        if($id){
+            User::where('id',$id)->delete();
+            session()->flash('message', 'Users Deleted Successfully.');
+        }
+    }
 }
